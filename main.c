46b10a30d0067dbf149cc30d95d8987306b8f173
sebseb7 <github@seb.exse.net>
2016-01-27 18:59:04 +0000

    testing

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 
 #include "main.h"
 #include "dmx_devices.h"
@@ -11,9 +13,16 @@ int main(int argc __attribute__((__unused__)), char *argv[] __attribute__((__unu
 	dmx_device_create_ledpar(16,0,"vorn rechts");
 	dmx_device_create_ledpar(24,0,"hinten links");
 	dmx_device_create_ledpar(32,0,"hinten rechts");
+	dmx_device_create_ledpar(32,0,"hinten rechts2");
 
 
 
+
+
+	for(unsigned int i = 0;i< dmx_get_device_count();i++)
+	{
+		printf("%ui %i %i %s\n",i,dmx_get_device_count(),dmx_get_device_byidx(i)->addr,dmx_get_device_byidx(i)->name);
+	}
 
 
 	return 0;
