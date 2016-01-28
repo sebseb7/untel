65db56868238cc5a25031d9121acad1c06de8e56
sebseb7 <github@seb.exse.net>
2016-01-28 03:11:12 +0000

    basic api

--- dmx_queue.c
+++ dmx_queue.c
@@ -1,4 +1,39 @@
+#include <stdlib.h>
+
 #include "dmx_queue.h"
 
 
+
+#define DMX_QUEUE_ALLOCATE_INITIAL 100
+#define DMX_QUEUE_ALLOCATE_STEP 10
+
+static struct dmx_queue* dmx_queue_list;
+
+static unsigned int queues_allocated=0;
+static unsigned int queues_inuse=0;
+
+
+
+struct dmx_queue* dmx_queue_add(void (*init)(void),void (*deinit)(void),void (*step)(unsigned int))
+{
+	if(0==queues_allocated)
+	{
+		dmx_queue_list = malloc(sizeof(*dmx_queue_list)*DMX_QUEUE_ALLOCATE_INITIAL);
+		queues_allocated=DMX_QUEUE_ALLOCATE_INITIAL;
+	}
+
+	if(queues_inuse == queues_allocated)
+	{
+		//implement realloc
+		exit(EXIT_FAILURE);
+	}
+	struct dmx_queue* queue = &dmx_queue_list[queues_inuse];
+	queues_inuse++;
+
+	queue->init = init;
+	queue->deinit = deinit;
+	queue->step = step;
+
+	return queue;
+}
 
--- dmx_queue.h
+++ dmx_queue.h
@@ -1,6 +1,14 @@
 #ifndef _DMX_QUEUE_H
 #define _DMX_QUEUE_H
 
+struct dmx_queue 
+{
+	void (*init)(void);
+	void (*deinit)(void);
+	void (*step)(unsigned int);
+};
+
+struct dmx_queue* dmx_queue_add(void (*init)(void),void (*deinit)(void),void (*step)(unsigned int));
 
 #endif
 
