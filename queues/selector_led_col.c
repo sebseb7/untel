27223a53a0c82ea9e85fd51575ebce6aa7c1e754
sebseb7 <github@seb.exse.net>
2016-02-04 19:43:42 +0100

    more colors

--- queues/selector_led_col.c
+++ queues/selector_led_col.c
@@ -10,6 +10,26 @@
 
 static struct dmx_set* set1;
 
+struct colornames {
+	const char* color;
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+};
+
+static struct colornames colornameslist[] = {
+
+	{"red"    ,255,0  ,0  },
+	{"green"  ,0  ,255,0  },
+	{"blue"   ,0  ,0  ,255},
+	{"yellow" ,255,100,0  },
+	{"magenta",255,0  ,203},
+	{"orange" ,255,15 ,0  },
+	{"rose"   ,255,23 ,23 },
+	{"cyan"   ,0  ,255,255},
+	{"white"  ,255,255,255}
+
+};
 
 
 static void init(void)
@@ -26,51 +46,31 @@ static void getname(__attribute__((__unused__)) unsigned int idx,__attribute__((
 
 static unsigned int getidbyname(char* name)
 {
-	if(strncmp(name,"red",DMX_NAME_LENGTH)==0)
+	for(unsigned int i=0;i< (sizeof(colornameslist)/sizeof(colornameslist[0]));i++)
 	{
-		return 1;
-	}
-	else if(strncmp(name,"blue",DMX_NAME_LENGTH)==0)
-	{
-		return 2;
-	}
-	else if(strncmp(name,"green",DMX_NAME_LENGTH)==0)
-	{
-		return 3;
+		if(strncmp(name,colornameslist[i].color,DMX_NAME_LENGTH)==0)
+		{
+			return i+1;
+		}
 	}
 	return 0;
 }
 
 
-static void position(__attribute__((__unused__)) unsigned int index)
+static void position(unsigned int index)
 {
-	unsigned int red=0;
-	unsigned int green=0;
-	unsigned int blue=0;
+	if(index > 	(sizeof(colornameslist)/sizeof(colornameslist[0])))
+		return;
 
-	switch(index)
-	{
-		case 1:
-			red=255;
-			break;
-		case 2:
-			blue=255;
-			break;
-		case 3:
-			green=255;
-			break;
-	
-	}
-		
-	set1->color[0]=red;
-	set1->color[1]=green;
-	set1->color[2]=blue;
+	set1->color[0]=colornameslist[index-1].red;
+	set1->color[1]=colornameslist[index-1].green;
+	set1->color[2]=colornameslist[index-1].blue;
 }
 
 
 static void constructor(void) CONSTRUCTOR_ATTRIBUTES
 static void constructor(void) {
-	
+
 	struct dmx_selector* selector = dmx_selector_add("LP COL",init,deinit,getname,getidbyname,position,1);
 
 	set1=malloc(sizeof(struct dmx_set));
@@ -79,7 +79,6 @@ static void constructor(void) {
 	set1->color[1]=0;
 	set1->color[2]=0;
 	dmx_selector_attach_set(selector,set1);
-
 }
 
 
