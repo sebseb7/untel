750e3111c3e58a6e3460efa0de0109c3b2415d8e
sebseb7 <github@seb.exse.net>
2016-03-31 04:59:41 +0200

    lua cue

--- queues/lua_queues.c
+++ queues/lua_queues.c
@@ -6,7 +6,9 @@ static void constructor(void) {
 
 	struct dmx_luaqueue* luaqueue = dmx_luaqueue_add("lua-par56");
 
-	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-004\")\nqueue_off(\"LED-SCENE-001\")",25*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-006\")\nqueue_off(\"LED-SCENE-001\")",26*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-005\")\nqueue_off(\"LED-SCENE-006\")",26*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-004\")\nqueue_off(\"LED-SCENE-005\")",25*10000,0,0,0);
 	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-002\")\nqueue_off(\"LED-SCENE-004\")",25*10000,0,0,0);
 	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-003\")\nqueue_off(\"LED-SCENE-002\")",25*10000,0,0,0);
 	dmx_luaqueue_addstep(luaqueue,"queue_on(\"LED-SCENE-001\")\nqueue_off(\"LED-SCENE-003\")",26*10000,0,0,0);
@@ -17,10 +19,12 @@ static void constructor(void) {
 
 	struct dmx_luaqueue* luaqueue2 = dmx_luaqueue_add("lua-strobe");
 
-	dmx_luaqueue_addstep(luaqueue2,"queue_on(\"STROBE-SCENE-002\")",9*10000,0,0,0);
-	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-002\")",9*10000,0,0,0);
-	dmx_luaqueue_addstep(luaqueue2,"queue_on(\"STROBE-SCENE-001\")",5*10000,0,0,0);
-	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-001\")",25*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-003\")\nqueue_on(\"STROBE-SCENE-002\")",9*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-002\")\nqueue_on(\"STROBE-SCENE-003\")",9*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-003\")\nqueue_on(\"STROBE-SCENE-001\")",6*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-001\")\nqueue_on(\"STROBE-SCENE-003\")",9*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-003\")\nqueue_on(\"STROBE-SCENE-004\")",9*10000,0,0,0);
+	dmx_luaqueue_addstep(luaqueue2,"queue_off(\"STROBE-SCENE-004\")\nqueue_on(\"STROBE-SCENE-003\")",9*10000,0,0,0);
 
 	dmx_luaqueue_activate(luaqueue2);
 
